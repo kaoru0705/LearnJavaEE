37ef7fe8cc194dc0f3bb01a69cf16cc95d063345
kaoru0705 <kaoru9875@gmail.com>
2025-11-19 22:40:50 +0900

    Update RegistController.java

--- site1118/src/main/java/com/ch/site1118/controller/RegistController.java
+++ site1118/src/main/java/com/ch/site1118/controller/RegistController.java
@@ -47,6 +47,8 @@ public class RegistController extends HttpServlet{
 		// 보통 드라이버는 java가 자체적으로 보유할 수 없다..(java 입장에서는 어떠한 db가 존재하는지 알 수 없다.)
 		// 따라서 드라이버 제작의 의무는 db제품을 판매한는 벤더사에게 있다.
 		
+		// JDBC 드라이버는 Class.forName() 메소드를 사용하여 동적 바인딩 방식으로 로드된다.
+		// 이때 매개변수로 해당 JDBC 드라이버의 이름을 입력해야 한다.
 		// jvm의 3가지 메모리 영역 중 Method 영역에 동적으로 클래스를 Load 시킴
 		// 보통은 jvm 자동으로 로드해주지만, 개발자가 원하는 시점에 원하는 클래스를 로드시킬 경우 아래와 같은
 		// Class 클래스가 static method인 forName() 메서드를 사용하기도 한다.
@@ -63,6 +65,7 @@ public class RegistController extends HttpServlet{
 		// 이 기술은 javaSE의 java.sql 패키지에서 주로 지원함
 		// 현재 우리가 개발중인 분야가 javaEE라면 javaEE는 이미 javaSE를 포함하고 있다.
 		
+		// jdbc:mysql://서버주소:포트번호/DB이름
 		String url = "jdbc:mysql://localhost:3306/java";
 		String user = "servlet";
 		String pass = "1234";
@@ -71,6 +74,8 @@ public class RegistController extends HttpServlet{
 		Connection con = null;		// finally에서 보이도록
 		PreparedStatement pstmt = null;		// finally에서 보이도록
 		try {
+			// getConnection() 매개변수로는 url, user, pass를 각각 설정한다.
+			// getConnection() 메소드는 DB서버와 연결하고 연결 정보를 Connection 객체에 저장하여 반환
 			con = DriverManager.getConnection(url, user, pass);
 			// Connection이란? 접속 성공 후 그 정보를 가진 객체이므로, 접속을 끊고 싶은 경우 이 객체를 이용하면 됨
 			// 예) con.close() 접속해제
@@ -84,7 +89,8 @@ public class RegistController extends HttpServlet{
 				// insert 문 수행
 				// JDBC 객체 중 쿼리수행을 담당하는 객체가 바로 PreparedStatement 인터페이스이다!
 				// 그리고 이 객체는 접속을 성공을 해야, 얻을 수 있다.(당연 이치=접속해야 쿼리문 수행할 수 있으니)
-				
+				// PreparedStatement 객체(여기서 pstmt)는 SQL문을 미리 컴파일하여 실행하기 전의 상태로 만들어 둔 후 실제 실행은 나중에 필요에 따라 여러 번 실행시킬 수 있다.
+				// 또한 PreparedStatement 객체로 생성되는 SQL문은 마치 함수처럼 매개변수를 설정하여 필요에 따라 매개변수의 값을 바꿔 실행되도록 할 수 있다.
 				pstmt=con.prepareStatement("insert into member(id, pwd, name) values('"+ id +"', '"+pwd+"', '"+name+"')");		// 쿼리문 준비
 				
 				// 준비된 쿼리문을 실행하자
