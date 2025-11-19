92ae1a0b5e7356f3c3d8076fb925453bf4510736
kaoru0705 <51065756+kaoru0705@users.noreply.github.com>
2025-11-19 15:33:37 +0900

    jdbc mysql

--- site1118/src/main/java/com/ch/site1118/controller/RegistController.java
+++ site1118/src/main/java/com/ch/site1118/controller/RegistController.java
@@ -1,14 +1,18 @@
 package com.ch.site1118.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.mysql.cj.xdevapi.Result;
 
 // 회원 등록 요청을 처리할 서블릿 클래스
 // HTTP 요청 방식 중, 클라이언트가 서버로 데이터를 전송해오는 방식은 POST 방식임
@@ -29,6 +33,12 @@ public class RegistController extends HttpServlet{
 		System.out.println("전송 받은 아이디는 "+ id);
 		System.out.println("전송 받은 패스워드는 "+ pwd);
 		System.out.println("전송 받은 이름은 "+ name);
+		
+		// 응답객체가 보유한 문자기반의 출력스트림에 개발자가 유저에게 전달하고 싶은 메시지를 보관하자
+		response.setContentType("text/html;");		// 브라우저에게 이 문서의 형식이 html 임을 알린다.
+		response.setCharacterEncoding("utf-8");		// 이 html에서 사용할 문자열에 대해 전세계 모든 언어를
+																	// 깨지지 않도록, UTF-8;
+		PrintWriter out = response.getWriter();
 		
 		// mysql에 넣어주기
 		
@@ -53,13 +63,15 @@ public class RegistController extends HttpServlet{
 		// 이 기술은 javaSE의 java.sql 패키지에서 주로 지원함
 		// 현재 우리가 개발중인 분야가 javaEE라면 javaEE는 이미 javaSE를 포함하고 있다.
 		
-		String url = "jdbc:mysql://localhost:3306/";
+		String url = "jdbc:mysql://localhost:3306/java";
 		String user = "servlet";
 		String pass = "1234";
 		
 		// try catch block shift alt z
+		Connection con = null;		// finally에서 보이도록
+		PreparedStatement pstmt = null;		// finally에서 보이도록
 		try {
-			Connection con = DriverManager.getConnection(url, user, pass);
+			con = DriverManager.getConnection(url, user, pass);
 			// Connection이란? 접속 성공 후 그 정보를 가진 객체이므로, 접속을 끊고 싶은 경우 이 객체를 이용하면 됨
 			// 예) con.close() 접속해제
 			// 주의) jdbc에서 데이터베이스에 접속 성공 여부를 판단할 때는 절대로 try문이 성공, catch문이 실패라고 생각하면 안 됨
@@ -68,9 +80,46 @@ public class RegistController extends HttpServlet{
 				System.out.println("접속 실패");
 			} else {
 				System.out.println("접속 성공");
+				
+				// insert 문 수행
+				// JDBC 객체 중 쿼리수행을 담당하는 객체가 바로 PreparedStatement 인터페이스이다!
+				// 그리고 이 객체는 접속을 성공을 해야, 얻을 수 있다.(당연 이치=접속해야 쿼리문 수행할 수 있으니)
+				
+				pstmt=con.prepareStatement("insert into member(id, pw, name) values('"+ id +"', '"+pwd+"', '"+name+"')");		// 쿼리문 준비
+				
+				// 준비된 쿼리문을 실행하자
+				int result = pstmt.executeUpdate();	// DML, 메서드 실행 후 반환되는 값은 이 메서드에 의해 영향을 받은 레코드 수가 반환됨..
+				// 따라서 1보다 작은 수가 반환되면, 이 쿼리에 이해 영향을 받은 레코드가 없으므로 수행 실패
+				if(result<1) {
+					System.out.println("등록 실패");
+					out.print("<script>"); 
+					out.print("alert('등록실패');");
+					out.print("</script>");
+				} else {
+					System.out.println("등록 성공");
+					// 웹브라우저에 성공 메시지 출력하기
+					out.print("<script>"); 
+					out.print("alert('등록성공');");
+					out.print("</script>");
+				}
 			}
 		} catch (SQLException e) { 
 			e.printStackTrace();
+		}finally {
+			if(pstmt != null) {		// 존재할 때만 닫음
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}	
+			}
+			if(con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 }
