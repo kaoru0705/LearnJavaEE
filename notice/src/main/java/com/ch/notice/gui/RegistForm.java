990d0fae5eb1bb98bb7cd20eaddd0d7104652be1
kaoru0705 <51065756+kaoru0705@users.noreply.github.com>
2025-11-28 16:48:29 +0900

    MemberDAO CRUD
    데이터베이스 연동을 전담하는 역할을 하는 객체

--- notice/src/main/java/com/ch/notice/gui/RegistForm.java
+++ notice/src/main/java/com/ch/notice/gui/RegistForm.java
@@ -14,6 +14,8 @@ import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
+import com.ch.notice.repository.MemberDAO;
+
 public class RegistForm extends JFrame{
 	
 	// 클래스가 보유한 멤버변수가 객체형일 경우 has a 관계
@@ -21,6 +23,7 @@ public class RegistForm extends JFrame{
 	JTextField writer;
 	JTextArea content;		// 내용 입력 박스
 	JButton bt;		// 등록버튼
+	MemberDAO dao;
 	
 	
 	public RegistForm() {
@@ -28,6 +31,7 @@ public class RegistForm extends JFrame{
 		writer = new JTextField(30);
 		content = new JTextArea(10, 30);
 		bt = new JButton("등록");
+		dao = new MemberDAO();
 		
 		// 컴포넌트를 부착하기 전에 레이아웃을 결정짓자. css div로 레이아웃을 적용하는 것과 비슷
 		setLayout(new FlowLayout());		// 수평이나 수직으로 흐르는 레이아웃
@@ -55,51 +59,12 @@ public class RegistForm extends JFrame{
 	
 	// 게시물 등록!!
 	public void regist() {
-		Connection con = null;
-		PreparedStatement pstmt = null;
+		int result = dao.regist(title.getText(), writer.getText(), content.getText());
 		
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			System.out.println("드라이버 로드 성공");
-			
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/java", "servlet", "1234");
-			System.out.println(con);
-			
-			String sql = "insert into notice(title, writer, content) values(?, ?, ?)";
-			
-			pstmt = con.prepareStatement(sql);
-			pstmt.setString(1, title.getText());
-			pstmt.setString(2,  writer.getText());
-			pstmt.setString(3,  content.getText());
-			
-			int result = pstmt.executeUpdate();		// DML 수행
-			if(result < 1) {
-				JOptionPane.showMessageDialog(this, "등록 실패");
-			}else {
-				JOptionPane.showMessageDialog(this, "등록 성공");
-			}
-			
-		} catch (ClassNotFoundException e) {
-			System.out.println("드라이버 로드 실패");
-			e.printStackTrace();
-		} catch (SQLException e) {
-			System.out.println("접속 실패");
-			e.printStackTrace();
-		}finally {
-			if(pstmt != null) {
-				try {
-					pstmt.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
-			if(con != null) {
-				try {
-					con.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
+		if(result < 1) {
+			JOptionPane.showMessageDialog(this, "실패");
+		}else {
+			JOptionPane.showMessageDialog(this, "성공");
 		}
 	}
 	
---
+++ notice/src/main/java/com/ch/notice/repository/MemberDAO.java
@@ -0,0 +1,66 @@
+package com.ch.notice.repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/*
+ * 이 클래스의 목적은?
+ * javaEE 기반의 애플리케이션이든, javaSE 기반의 애플리케이션 데이터베이스를 연동하는 비즈니스 로직은 동일하다.
+ * 따라서 유지보수성을 고려하여 여러 플랫폼에서 재사용할 수 있는 객체를 정의
+ * 특히 로직 객체 중 오직 데이터베이스 연동을 전담하는 역할을 하는 객체를 가리켜 애플리케이션 설계 분야에서는
+ * DAO (Data Access Object) - DB에 테이블이 만일 5개라면 DAO도 1:1 대응하여 5개를 만들어야 한다.
+ * 특히 수행하는 데이터베이스의 테이블에 데이터를 처리하는 업무를 가리켜 Create(=insert) Read(select) Update Delete 
+ * 
+ */
+public class MemberDAO {
+	// 게시물 등록!!
+	public int regist(String title, String writer, String content) {
+		int result = 0;	// insert 후 성공인지 실패인지를 판단할 수 있는 반환값
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("드라이버 로드 성공");
+			
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/java", "servlet", "1234");
+			System.out.println(con);
+			
+			String sql = "insert into notice(title, writer, content) values(?, ?, ?)";
+			
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, title);
+			pstmt.setString(2,  writer);
+			pstmt.setString(3,  content);
+			
+			result = pstmt.executeUpdate();		// DML 수행
+			
+			
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로드 실패");
+			e.printStackTrace();
+		} catch (SQLException e) {
+			System.out.println("접속 실패");
+			e.printStackTrace();
+		}finally {
+			if(pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if(con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return result;
+	}
+
+}
