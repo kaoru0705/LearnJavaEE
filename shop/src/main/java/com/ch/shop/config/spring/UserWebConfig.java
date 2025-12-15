2e162c610e0bba02848827694af4f39cc0f03c28
kaoru0705 <51065756+kaoru0705@users.noreply.github.com>
2025-12-15 17:49:54 +0900

    board spring으로 만드는 중

--- shop/src/main/java/com/ch/shop/config/spring/UserWebConfig.java
+++ shop/src/main/java/com/ch/shop/config/spring/UserWebConfig.java
@@ -1,9 +1,19 @@
 package com.ch.shop.config.spring;
 
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jndi.JndiTemplate;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import com.ch.shop.controller.shop.BoardController;
 
@@ -23,5 +33,66 @@ import com.ch.shop.controller.shop.BoardController;
 @ComponentScan(basePackages = "com.ch.shop.controller")
 
 public class UserWebConfig {
-
+	
+	// DispatcherServlet이 하위 컨트롤러로부터 반환받은 결과 페이지에 대한 정보는 사실 완전한 JSP 경로가 아니므로,
+	// 이를 해석할 수 있는 자인 ViewResolver에게 맡겨야 하는데, 이 ViewResolver 중 유달리 접두어와 접미어 방식을 이해하는 
+	// ViewResolver를 InternalResourceViewResolver라고 한다.. 개발자는 이 객체에게 접두어와 접미어을 사전에 등록해 놓아야 한다.
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver rv = new InternalResourceViewResolver();
+		// 접두어 등록
+		rv.setPrefix("/WEB-INF/views/");
+		// 접미어 등록
+		rv.setSuffix(".jsp");
+		return rv;
+	}
+	
+	/*
+	 * 스프링이 MVC 프레임워크 중 컨트롤러 영역만을 지원하는 것이 아니라, 데이터베이스 관련 제어도 지원하므로,
+	 * 지금까지 순수하게 사용해왔던 mybatis를 스프링이 지원하는 mybatis로 전환해본다.
+	 * 스프링이 지원하는 데이터 연동 기술을 사용하려면, spring jdbc 라이브러리를 추가해야 한다...
+	 * spring jdbc 검색 
+	 */
+	
+	/*----------------------------------------------------------------
+	 * 1) 개발자가 사용하고 싶은 데이터소스를 결정
+	 * 		- 톰캣이 지원하는 JNDI를 사용할 예정
+	 *----------------------------------------------------------------*/
+	public DataSource dataSource() throws NamingException{
+		JndiTemplate jndi = new JndiTemplate();
+		return jndi.lookup("java:comp/env/jndi/mysql", DataSource.class);
+	}
+	
+	/*----------------------------------------------------------------
+	 * 2) 트랜잭션 매니저 등록
+	 * 		- 스프링은 개발자가 사용하는 기술이 JDBC, Mybatis, Hibernate, JPA 이건 상관없이
+	 * 			일관된 방법으로 트랜잭션을 처리할 수 있는 방법을 제공해주는데, 개발자는 자신이 사용하는 기술에 따라
+	 * 			적절한 트랜잭션 매니저를 등록해야 한다.
+	 * 			예) JDBC 사용 시 - DataSourceTransactionManager를 빈으로 등록해야 함
+	 * 			예) Hibernate 사용 시 - HibernateTransactionManager를 빈으로 등록해야 함
+	 * 			예) Mybatis 사용 시 - DataSourceTransactionManager를 빈으로 등록해야 함
+	 * 										특히 Mybatis의 경우 JDBC와 동일한 DataSourceTransactionManager를 사용하는 이유는?
+	 * 										사실 Mybatis는 내부적으로, JDBC를 사용하기 때문임...
+	 * 			그리고 이 모든 트랜잭션 매니저의 최상단 객체가 바로 PlatformTransactionManager 이다.
+	 *----------------------------------------------------------------*/
+	@Bean
+	public PlatformTransactionManager transactionManager() {
+		return new DataSourceTransactionManager();
+	}
+	
+	
+	/*----------------------------------------------------------------
+	 * 3) SqlSession을 관리하는 mybatis의 SqlSessionFactory를 빈으로 등록
+	 *----------------------------------------------------------------*/
+	@Bean
+	public SqlSessionFactory sqlSessionFactory() throws Exception{
+		// 순수 mybatis 프레임워크 자체에서 지원하는 객체가 아니라, mybatis-spring에서 지원하는 객체인
+		// SqlSessionFactoryBean (끝에 Bean)을 이용하여 설정 xml 파일을 로드한다.
+		
+		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+		
+		// 패키지에 포함된 파일의 유형이 클래스가 아닌 경우 더 이상 패키지로 표현하지 말고, 일반 디렉토리로 취급해야 한다..
+		sqlSessionFactoryBean.setConfigLocation(new ClassPathResource("com/ch/shop/config/mybatis/config.xml"));
+		return sqlSessionFactoryBean.getObject();
+	}
 }
--- shop/src/main/java/com/ch/shop/controller/shop/BoardController.java
+++ shop/src/main/java/com/ch/shop/controller/shop/BoardController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.ch.shop.dto.Board;
+
 /*
  * 우리가 제작한 MVC 프레임웍에서는 모든 요청마다 1:1 대응하는 컨트롤러를 매핑하는 방식이었으나,
  * 스프링 MVC는 예를 들어 게시판 1개에 대한 목록, 쓰기, 상세보기, 수정, 삭제에 대해 하나의 컨트롤러로 처리가 가능함
@@ -13,15 +15,59 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class BoardController {
 	
+	// 글쓰기 폼 요청 처리 - jsp가 WEB-INF 밑의 위치하였으므로, 브라우저에서 jsp를 직접 접근할 수 없다.
+	// 따라서 아래의 컨트롤러의 메서드에서 /board/write.jsp를 매핑 걸자
+	@RequestMapping("/board/registform")
+	public ModelAndView registForm() {
+		// 3단계: 일 시킬 게 없다!!
+		// 4단계: 없다.
+		
+		/*
+		 * "/WEB-INF/views/board/write.jsp"이렇게 직접 보낼 순 없어서 /board/result/registform 이렇게 key 값을 보냈지만 이것도 귀찮다.
+		 * DispatcherServlet에게 완전한 jsp 경로를 반환하게 되면, 파일명이 바뀔 때, 이 클래스도 영향을 받으므로
+		 * 무언가 jsp를 대신할만한 키 등을 구상해야 하는데, 스프링의 창시자인 로드 존슨은 접두어, 접미어를 활용하는 방식을 고안해냄
+		 * 따라서 개발자는 전체 파일명 경로 중 변하지 않는다고 생각하는 부분에 대해 접두어, 집미어를 규칙으로 정하여 알맹이만 반환하는
+		 * 방법을 쓰면 된다. 이때 하위컨트롤러가 DispatcherServlet에게 정보를 반환할 때는 String형으로 반환해도 되지만, 
+		 * ModelAndView라는 객체를 이용할 수도 있다.
+		 * 참고로 ModelAndView에는 데이터를 담을 때는 Model객체에 자동으로 담기고, jsp 접두어, 접미어를 제외한 문자열을 넣어둘 때는
+		 * View 객체에 담기는데, ModelAndView는 이 두 객체를 합쳐놓은 객체임. 
+		 * 여기서는 /WEB-INF/views/가 접두어 .jsp가 접미어
+		 */
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("board/write");
+		return mav;	// /board/result/registform
+	}
+	
+
+	
+	
 	// 글 목록 페이지 요청 처리
 	// 이 URL 요청이 들어오면 이 메서드를 실행하라고 스프링에게 알려주는 주소표
 	@RequestMapping("/board/list")
 	public ModelAndView getList() {
+		// 3단계 수행
 		System.out.println("클라이언트의 목록 요청 감지");
+		
+		// 4단계: 결과 저장..
+		
 		return null;
 	}
 	
 	// 글 쓰기 요청 처리
+	// 메서드의 매개변수에 VO(Value Object)로 받을 경우
+	// 스프링에서 자체적으로 자동 매핑에 의해 파라미터값들을 채워넣는다.
+	// 단, 전제 조건? 파라미터명과 VO의 변수명이 반드시 일치해야 한다.
+	// DTO와 VO는 비슷하긴 하지만, DTO는 테이블을 반영한 객체이다 보니
+	// 클라이언트에 노출되지 않도록 하는 것이 좋기 때문에, 단순히 클라이언트의 파라미터를
+	// 받는 것이 목적이라면, DTO보다는 VO를 사용해야 한다..
+	@RequestMapping("/board/regist")
+	public ModelAndView regist(Board board) {
+		System.out.println("제목은 " + board.getTitle());
+		System.out.println("작성자는 " + board.getWriter());
+		System.out.println("내용은 " + board.getContent());
+		
+		return null;
+	}
 	
 	// 글 상세보기 요청 처리
 	
